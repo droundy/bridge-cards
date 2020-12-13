d7f7b901dcc877ee67d652ba29bc78c0652d107d
David Roundy <daveroundy@gmail.com>
2020-12-13 13:02:16 -0800

    add benhmarking

---
+++ benches/bench.rs
@@ -0,0 +1,22 @@
+use easybench::{bench};
+
+fn main() {
+    println!(
+        "deal 13 cards {}",
+        bench(|| bridge_deck::Cards::ALL.pick(13))
+    );
+    println!(
+        "deal 13 cards (cardpack) {}",
+        bench(|| cardpack::Pack::spades_deck().cards().shuffle().draw(13))
+    );
+    println!();
+    println!(
+        "deal 1 card {}",
+        bench(|| bridge_deck::Cards::ALL.pick(1))
+    );
+    println!(
+        "deal 1 card (cardpack) {}",
+        bench(|| cardpack::Pack::spades_deck().cards().shuffle().draw(1))
+    );
+    println!();
+}
