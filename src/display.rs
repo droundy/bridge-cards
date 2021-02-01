4977758a766fed9408a5db7b5f92749b81592b17
David Roundy <daveroundy@gmail.com>
2021-02-01 05:43:16 -0800

    fix test

--- src/display.rs
+++ src/display.rs
@@ -30,7 +30,7 @@ impl DisplayAs<UTF8> for Cards {}
 #[test]
 fn disp() {
     assert_eq!("🃑", &format_as!(UTF8, Card::CA));
-    assert_eq!(r#"<span class="clubs">🃑</span>"#, &format_as!(HTML, Card::CA));
+    assert_eq!(r#"<span class="clubs">♣A</span>"#, &format_as!(HTML, Card::CA));
 
     // println!("{}", format_as!(UTF8, Cards::ALL));
     // assert_eq!(r#"<span class="clubs">🃑</span>"#, &format_as!(UTF8, Cards::ALL));
